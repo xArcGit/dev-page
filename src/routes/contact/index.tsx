e4b12d1524f51e73d00e7f5cf1afab532c713a7a
xarcgit <yasouo@proton.me>
2024-11-21 21:13:39 +0530

    added neaded components

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -3,14 +3,8 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
 	return (
 		<>
-			<div role="presentation" class="ellipsis" />
-			<div role="presentation" class="ellipsis ellipsis-purple" />
-
 			<div class="container container-center container-spacing-xl">
-				<h3>
-					You can <span class="highlight">count</span>
-					<br /> on me
-				</h3>
+				<h3>Contact</h3>
 			</div>
 		</>
 	);
--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -4,14 +4,8 @@ import type { DocumentHead } from "@builder.io/qwik-city";
 export default component$(() => {
 	return (
 		<>
-			<div role="presentation" class="ellipsis" />
-			<div role="presentation" class="ellipsis ellipsis-purple" />
-
 			<div class="container container-center container-spacing-xl">
-				<h3>
-					You can <span class="highlight">count</span>
-					<br /> on me
-				</h3>
+				<h3>Projects</h3>
 			</div>
 		</>
 	);
